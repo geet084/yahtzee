aa744934a0a1981c195c6dfa1534f894b69eb13d
Travis Gee <geet084@gmail.com>
2019-09-22 17:02:16 -0600

    Adds game class & methods

--- game.js
+++ game.js
@@ -1,8 +1,53 @@
+import Player from './player.js';
+import Points from './points.js';
+
 export default class Game {
   constructor() {
-    this.round = 1
+    this.round = 13
+    this.players = new Player()
+    this.rollCount = 0
+    this.heldDice = [false, false, false, false, false]
+    this.dice = [0, 0, 0, 0, 0]
+    this.bonus = 35
+    this.points = 0
+    this.numYahtzee = 0
   }
-  start() {
-    console.log('start', this.round);
+
+  updateScore() {
+    this.players.score += this.points
+    this.round--
+    this.points = 0
+    this.rollCount = 0
+    this.heldDice = [false, false, false, false, false]
+    return this.players.score
   }
+
+  rollDice() {
+    this.rollCount++
+    newDiceRoll().map((die, i) => {
+      const notHeld = this.heldDice[i] === false
+      if (notHeld) this.dice[i] = die
+    })
+    return this.dice
+  }
+
+  getPoints(target) {
+    let points = new Points(this.dice, this.numYahtzee)
+    this.points = points.calculate(target)
+    
+    if (this.points >= 50) this.numYahtzee += 1
+  }
+
+  addBonus() {
+    this.players.score += this.bonus
+    this.bonus = 0
+  }
+}
+
+function newDiceRoll() {
+  let dice = []
+  for (let i = 0; i < 5; i++) {
+    dice.push(Math.floor(Math.random() * 6) + 1)
+  }
+  return dice
 }
