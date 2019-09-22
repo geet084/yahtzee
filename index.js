0b292b0fa5b30675325643727dab1847e6ff9ebd
Travis Gee <geet084@gmail.com>
2019-09-22 17:00:46 -0600

    Adds methods on index as game controller

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 import Game from './game.js';
+import * as dom from './domUpdates.js'
 
 let game = new Game();
-game.start()
+
+export function handleScore() {
+  const score = game.updateScore()
+  dom.checkForUpperBonus()
+  dom.updateScore(score)
+  dom.nextTurn()
+  checkForGameEnd()
+}
+
+export function handleBonus(total) {
+  if (total >= 63) game.addBonus()
+}
+
+export function handleRoll() {
+  dom.addScoreBoxEventListeners()
+
+  if (game.rollCount < 3) {
+    let dice = game.rollDice()
+    dom.updateDiceArea(dice, game.rollCount)
+  }
+}
+
+export function toggleHeldDice(index) {
+  game.heldDice[index] = !game.heldDice[index]
+}
+
+export function getRolledPoints(target) {
+  game.getPoints(target)
+  return game.points
+}
+
+function checkForGameEnd() {
+  if (game.round === 0) dom.endGame()
+}
