4b4d0f984ec3ee375bdcfda8fbcaa974a82d03eb
Travis Gee <geet084@gmail.com>
2019-10-15 18:08:20 -0600

    Adds initial random computer scoring

--- game.js
+++ game.js
@@ -4,22 +4,39 @@ import Points from './points.js';
 export default class Game {
   constructor() {
     this.round = 13
-    this.players = new Player()
+    this.currentPlayer = 'p1';
+    this.players = { p1: new Player('human'), p2: new Player('comp') }
     this.rollCount = 0
     this.heldDice = [false, false, false, false, false]
     this.dice = [0, 0, 0, 0, 0]
     this.bonus = 35
     this.points = 0
     this.numYahtzee = 0
+    this.compTarget = 'cx5'
   }
 
   updateScore() {
-    this.players.score += this.points
-    this.round--
+    this.players[this.currentPlayer].score += this.points
     this.points = 0
-    this.rollCount = 0
     this.heldDice = [false, false, false, false, false]
-    return this.players.score
+    this.rollCount = 0
+    if (this.currentPlayer === 'p1') {
+      this.round--
+      return { player: this.currentPlayer, score: this.players[this.currentPlayer].score }
+    }
+  }
+
+  compTurn() {
+    let opt = ['cu1', 'cu2', 'cu3', 'cu4', 'cu5', 'cu6', 'cx3', 'cx4', 'cfh', 'css', 'cls', 'cx5', 'cch']
+    let thing = Math.floor(Math.random() * 13) + 1
+    this.compTarget = opt[thing]
+    let num = Math.floor(Math.random() * 5) + 1
+    this.players[this.currentPlayer].score += num
+    return { points: num, score: this.players[this.currentPlayer].score }
+  }
+
+  nextPlayer() {
+    this.currentPlayer = this.currentPlayer === 'p1' ? 'p2' : 'p1'
   }
 
   rollDice() {
@@ -34,12 +51,12 @@ export default class Game {
   getPoints(target) {
     let points = new Points(this.dice, this.numYahtzee)
     this.points = points.calculate(target)
-    
+
     if (this.points >= 50) this.numYahtzee += 1
   }
 
   addBonus() {
-    this.players.score += this.bonus
+    this.players[this.currentPlayer].score += this.bonus
     this.bonus = 0
   }
 }
