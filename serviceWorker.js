b313bf06b58400af95205d7eeff16dccf82ee9c4
Travis Gee <geet084@gmail.com>
2019-11-18 20:19:26 -0700

    Updates version with help boxes

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = 'version 1.14'
+const cacheName = 'version 1.15'
 self.addEventListener("install", function (event) {
   // console.log('WORKER: install event in progress.');
   event.waitUntil(
